f0d09cb009222ac7d13c6904c372fa99da05e7d1
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-15 23:37:36 -0300

    Criado seeder para alimentar table users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name'              =>  Str::random(10),
+            'email'             =>  Str::random(10).'@gmail.com',
+            'email_verified_at' => now(),
+            'password'          =>  Hash::make('password'),
+            'remember_token'    => Str::random(10),
+        ]);
+    }
+}

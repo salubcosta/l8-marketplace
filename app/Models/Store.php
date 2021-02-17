7966e72ca391548178cc8990504e339f6e3cd645
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-16 23:28:27 -0300

    Criado model de store

---
+++ app/Models/Store.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Store extends Model
+{
+    use HasFactory;
+
+    /**
+     * Uma forma de informar manualmente o nome da tabela no DB
+     * protected $table = 'nomeTable';
+     */
+    
+}

7817c43f5dcb56e7fe3d18a6db376f85b957b215
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-16 23:32:26 -0300

    varios testes, buscas, add e update, active record e mass assignment e update

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,59 @@ Route::get('/', function () {
     $helloWorld = 'Hello World.';
     return view('welcome', compact('helloWorld'));
 });
+
+Route::get('/model', function(){
+    
+    /*  
+        Utilizando o recurso chamado Active Record
+        $user = new \App\Models\User();
+        $user->name = 'Salumao';
+        $user->email= 'salubcosta@gmail.com';
+        $user->password= bcrypt('123');
+        return \App\Models\User::all();
+    */
+
+    /*
+        $user = \App\Models\User::find(81);
+        $user->name = 'Salumão Editado';
+        $user->save();
+        return \App\Models\User::all();
+    */
+
+    /**
+     * Retorna todos os usuários
+     * return \App\Models\User::All(); 
+     * 
+     * Retorna usuários específico
+     * return \App\Models\User::find(81);
+     * 
+     * Retorno com where: 
+     *             +get     - retorna array
+     *             +first   - retorna um objeto
+     * return \App\Models\User::where('name', 'Salumao Editado')->get();   # collation
+     * return \App\Models\User::where('name', 'Salumao Editado')->first(); # object
+     * 
+     * Paginação
+     * return \App\Models\User::paginate(10);
+    */
+    
+    /*
+        Mass assignment - Atribuição em massa
+        $user = \App\Models\User::create([
+            'name'  => 'Salumão Barbosa',
+            'email' => 'salu.tj@hotmail.com',
+            'password' => bcrypt('123')
+        ]);
+
+        Mass update - Atualização em massa
+        $user = \App\Models\User::find(83);
+        $returnAction  = $user->update([
+            'name'  => 'Salumão BC',
+            'email' => 'salubcosta@outlook.com',
+            'password' => bcrypt('123')
+        ]);
+        dd($returnAction); # true or false
+    */
+    
+    return \App\Models\User::paginate(10);
+});

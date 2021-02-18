c1bcfd3e2bbd2ba5822af5eff1592d78077085ca
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-17 23:42:01 -0300

    relacionamento de produto com store e category e propriedade fillable

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description', 'body', 'price', 'slug'];
+
+    /**
+     * Relacionamento pertence a: store
+     */
+    public function store()
+    {
+        return $this->belongsTo(Store::class);
+    }
+
+    /**
+     * Relacionamento produtos pertencem a muitas: category
+     */
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }

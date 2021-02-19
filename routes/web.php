9a9800d64887481657ec8d691d727fc50e1e1ec1
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-18 23:54:09 -0300

    criando varias rotas para CRUD de store

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\StoreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -73,3 +74,15 @@ Route::get('/model', function(){
     
     return \App\Models\User::paginate(10);
 });
+
+Route::prefix('admin')->group(function(){
+    Route::prefix('stores')->name('admin.stores.')->group(function(){
+        Route::get('/', [StoreController::class, 'index'])->name('index');
+        Route::get('/create', [StoreController::class, 'create'])->name('create');
+        Route::post('/store', [StoreController::class, 'store'])->name('store');
+        Route::get('/{store}/edit', [StoreController::class, 'edit'])->name('edit');
+        Route::put('/update/{store}', [StoreController::class, 'update'])->name('update');
+        Route::get('/destroy/{store}',[StoreController::class, 'destroy'])->name('destroy');
+    });
+});
+

6bac56e4ea5a365d13171ad696a7f3e207a7234e
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-19 23:22:21 -0300

    Refatorado rotas para Stores e adicionado referencia para Produtos, agora Stores e Products sao carregados como recursos

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\StoreController;
+use App\Http\Controllers\Admin\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -75,14 +76,16 @@ Route::get('/model', function(){
     return \App\Models\User::paginate(10);
 });
 
-Route::prefix('admin')->group(function(){
-    Route::prefix('stores')->name('admin.stores.')->group(function(){
-        Route::get('/', [StoreController::class, 'index'])->name('index');
-        Route::get('/create', [StoreController::class, 'create'])->name('create');
-        Route::post('/store', [StoreController::class, 'store'])->name('store');
-        Route::get('/{store}/edit', [StoreController::class, 'edit'])->name('edit');
-        Route::put('/update/{store}', [StoreController::class, 'update'])->name('update');
-        Route::get('/destroy/{store}',[StoreController::class, 'destroy'])->name('destroy');
-    });
+Route::prefix('admin')->name('admin.')->group(function(){
+    // Route::prefix('stores')->name('stores.')->group(function(){
+    //     Route::get('/', [StoreController::class, 'index'])->name('index');
+    //     Route::get('/create', [StoreController::class, 'create'])->name('create');
+    //     Route::post('/store', [StoreController::class, 'store'])->name('store');
+    //     Route::get('/{store}/edit', [StoreController::class, 'edit'])->name('edit');
+    //     Route::put('/update/{store}', [StoreController::class, 'update'])->name('update');
+    //     Route::get('/destroy/{store}',[StoreController::class, 'destroy'])->name('destroy');
+    // });
+    Route::resource('stores', StoreController::class);
+    Route::resource('products', ProductController::class);
 });
 

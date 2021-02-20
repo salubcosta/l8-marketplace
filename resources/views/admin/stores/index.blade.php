203961f228b5a9d38e5a259220c0d03b01997da5
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-19 23:25:16 -0300

    Correcao da funcionalidade delete, agora utilizando o metodo http para esta rota

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -15,8 +15,14 @@
                 <td align="right">{{$store->id}}</td>
                 <td>{{$store->name}}</td>
                 <td align="right">
-                    <a href="{{route('admin.stores.edit',['store'=>$store->id])}}" class="btn btn-sm btn-primary">Editar</a>
-                    <a href="{{route('admin.stores.destroy',['store'=>$store->id])}}" class="btn btn-sm btn-danger">Remover</a>
+                    <div class="btn-group">
+                        <a href="{{route('admin.stores.edit',['store'=>$store->id])}}" class="btn btn-sm btn-primary">Editar</a>
+                        <form action="{{route('admin.stores.destroy',['store'=>$store->id])}}" method="POST">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="ml-1 btn btn-sm btn-danger">Remover</a>
+                        </form>
+                    </div>
                 </td>
             </tr>
             @endforeach

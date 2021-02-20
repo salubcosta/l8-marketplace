008ec695a7b3e1e5ac969520e5a79e328f97b17a
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-19 23:21:12 -0300

    Controller responsavel pelo CRUD de produtos

---
+++ app/Http/Controllers/Admin/ProductController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    private  $product;
+
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = $this->product->paginate(10);
+
+        return view('admin.products.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $stores = \App\Models\Store::all(['id', 'name']);
+        return view('admin.products.create', compact('stores'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        $store = \App\Models\Store::find($data['store']);
+
+        $store->products()->create($data);
+
+        flash('Registro salvo!')->success();
+        return redirect()->route('admin.products.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = $this->product->find($id);
+
+        return view('admin.products.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+
+        $product = $this->product->find($id);
+
+        $product->update($data);
+
+        flash('Registro atualizado!')->success();
+        return redirect()->route('admin.products.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = $this->product->find($id);
+        $product->delete();
+
+        flash('Registro excluído!')->success();
+        return redirect()->route('admin.products.index');
+    }
+}

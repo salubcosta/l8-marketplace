9654ffcc66841e0d2a374f810ba676ecc4945e19
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-22 23:49:08 -0300

    refatorado para usuario ter apenas uma loja

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -1,19 +1,22 @@
 @extends('layouts.app')
 @section('content')
-    <a href="{{route('admin.stores.create')}}" class="btn btn-success">Nova loja</a>
+    @if(!$store)
+        <a href="{{route('admin.stores.create')}}" class="btn btn-success mb-4">Nova loja</a>
+    @endif
     <table class="table table-striped">
         <thead>
             <tr>
                 <th>ID</th>
-                <th align="left">Loja</th>
-                <th align="right">Actions</th>
+                <th>Loja</th>
+                <th>Total de Produtos</th>
+                <th style="text-align: right">Actions</th>
             </tr>
         </thead>
         <tbody>
-            @foreach ($stores as $store)
             <tr>
-                <td align="right">{{$store->id}}</td>
+                <td>{{$store->id}}</td>
                 <td>{{$store->name}}</td>
+                <td>{{$store->products->count()}}</td>
                 <td align="right">
                     <div class="btn-group">
                         <a href="{{route('admin.stores.edit',['store'=>$store->id])}}" class="btn btn-sm btn-primary">Editar</a>
@@ -25,8 +28,6 @@
                     </div>
                 </td>
             </tr>
-            @endforeach
         </tbody>
     </table>
-    {{$stores->links()}}
 @endsection

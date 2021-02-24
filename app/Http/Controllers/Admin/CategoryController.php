ea19453167022169fc04a153893c678cf4d1640a
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-23 23:37:15 -0300

    Controller de Categorias

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Http\Requests\CategoryRequest;
+
+class CategoryController extends Controller
+{
+    private $category;
+
+    public function __construct(Category $category)
+    {
+        $this->category = $category;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categories = $this->category->paginate(10);
+        return view('admin.categories.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CategoryRequest $request)
+    {
+        $data = $request->all();
+        $data['slug'] = $data['name'];
+        $this->category->create($data);
+
+        flash('Registro salvo!')->success();
+        return redirect()->route('admin.categories.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $category = $this->category->find($id);
+
+        return view('admin.categories.edit', compact('category'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CategoryRequest $request, $id)
+    {
+        $data = $request->all();
+
+        $category = $this->category->find($id);
+
+        $category->update($data);
+
+        flash('Registro atualizado!')->success();
+        return redirect()->route('admin.categories.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category = $this->category->find($id);
+        $category->delete();
+
+        flash('Registro excluído!')->success();
+        return redirect()->route('admin.categories.index');
+    }
+}

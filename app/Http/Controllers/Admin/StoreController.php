9a9f1461217e5af31864c68702529effab749f80
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-26 23:30:41 -0300

    Implementado updaload de imagem com traits para store e update

--- app/Http/Controllers/Admin/StoreController.php
+++ app/Http/Controllers/Admin/StoreController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-// use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use App\Http\Requests\StoreRequest;
+use App\Traits\UploadTrait;
 
 class StoreController extends Controller
 {
+    use UploadTrait;
+
     public function __construct()
     {
         $this->middleware('user.has.store')->only(['create', 'store']);
@@ -26,7 +29,13 @@ class StoreController extends Controller
 
     public function store(StoreRequest $request){
         $data = $request->all();
-        $data['slug'] = $data['name']; # Trabando slug
+        $data['slug'] = $data['name']; # Falta trabalhar o slug
+
+        if($request->hasFile('logo'))
+        {
+           $data['logo'] = $this->imageUpload($request->file('logo'));
+        }
+
         auth()->user()->store()->create($data);
 
         flash('Registro salvo')->success();
@@ -43,6 +52,16 @@ class StoreController extends Controller
         $data = $request->all();
 
         $store = \App\Models\Store::find($store);
+
+        if($request->hasFile('logo'))
+        {
+            if(Storage::disk('public')->exists($store->logo))
+            {
+                Storage::disk('public')->delete($store->logo);
+            }
+            $data['logo'] = $this->imageUpload($request->file('logo'));
+        }
+
 
         $store->update($data);
 

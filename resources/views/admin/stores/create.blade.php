b6b172fb2a4dee93be3d667514b2a3928e84520f
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-26 23:32:55 -0300

    html de upload de logo e validacao

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1>Criar Loja</h1>
 
-    <form action="{{route('admin.stores.store')}}" method="POST">
+    <form action="{{route('admin.stores.store')}}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('post')
         <div class="form-group">
@@ -40,6 +40,16 @@
             <label>Celular/Whatsapp</label>
             <input type="text" name="mobile_phone" class="form-control @error('mobile_phone') is-invalid @enderror" value={{old('mobile_phone')}}>
             @error('mobile_phone')
+                <div class="invalid-feedback">
+                    {{$message}}
+                </div>
+            @enderror
+        </div>
+
+        <div class="form-group">
+            <label>Logo</label>
+            <input type="file" name="logo" class="form-control @error('logo') is-invalid @enderror">
+            @error('logo')
                 <div class="invalid-feedback">
                     {{$message}}
                 </div>

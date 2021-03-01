a1173cd5230c35c88eddea02f3fc9d083ffe1937
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-28 22:40:43 -0300

    update inforcacoes de produtos na view welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.front')
 
 @section('content')
+<div class="row mb-4">
+    @foreach ($products as $key => $product)
+    <div class="col-md-4">
+        <div class="card">
+            @if($product->photos->count())
+                <img src="{{asset('storage/' . $product->photos->first()->image)}}" alt="" class="card-img-top">
+            @else
+                <img src="{{asset('assets/img/no-photo.jpg')}}" alt="" class="card-img-top">
+            @endif
+            <div class="card-body">
+                <h2 class="card-title">{{$product->name}}</h2>
+                <p class="card-text">{{$product->description}}</p>
+                <h3>
+                    R$ {{number_format($product->price, '2', ',', '.')}}
+                </h3>
+            </div>
 
+            <a href="{{route('product.single', ['slug'=>$product->slug])}}" class="btn btn-success">
+                Ver Produto
+            </a>
+        </div>
+    </div>
+    @if(($key + 1)%3==0) </div><div class="row mb-4"> @endif
+    @endforeach
+</div>
 @endsection

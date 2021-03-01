328219c3cc55ee166193c01a05a3d90f87c8b261
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-28 21:07:19 -0300

    adicionado blade do front do marketplace

---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Marketplace L6</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <style>
+        .front.row {
+            margin-bottom: 40px;
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="margin-bottom: 40px;">
+
+    <a class="navbar-brand" href="{{route('home')}}">Marketplace</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item @if(request()->is('/')) active @endif">
+                <a class="nav-link" href="{{route('home')}}">Home <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+
+    @auth
+           <ul class="navbar-nav mr-auto">
+                    <li class="nav-item @if(request()->is('admin/stores*')) active @endif">
+                        <a class="nav-link" href="{{route('admin.stores.index')}}">Lojas <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item @if(request()->is('admin/products*')) active @endif">
+                        <a class="nav-link" href="{{route('admin.products.index')}}">Produtos</a>
+                    </li>
+                    <li class="nav-item @if(request()->is('admin/categories*')) active @endif">
+                        <a class="nav-link" href="{{route('admin.categories.index')}}">Categorias</a>
+                    </li>
+                </ul>
+
+                <div class="my-2 my-lg-0">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#" onclick="event.preventDefault();
+                                                                  document.querySelector('form.logout').submit(); ">Sair</a>
+
+                            <form action="{{route('logout')}}" class="logout" method="POST" style="display:none;">
+                                @csrf
+                            </form>
+                        </li>
+                        <li class="nav-item">
+                            <span class="nav-link">{{auth()->user()->name}}</span>
+                        </li>
+                    </ul>
+                </div>
+        @endauth
+
+    </div>
+</nav>
+
+<div class="container">
+    @include('flash::message')
+    @yield('content')
+</div>
+
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+</body>
+</html>

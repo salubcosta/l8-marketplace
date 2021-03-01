88db245c800972918ef13cdb11dfa658aaf53c36
Salumao Barobsa da Costa <salubcosta@gmail.com>
2021-02-28 22:41:13 -0300

    rota para single adicionada

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Admin\StoreController;
 use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\ProductPhotoController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,80 +17,12 @@ use App\Http\Controllers\Admin\ProductPhotoController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/home', function () {
-    $helloWorld = 'Hello World.';
-    return view('welcome', compact('helloWorld'));
-})->name('home');
-
-Route::get('/model', function(){
-    
-    /*  
-        Utilizando o recurso chamado Active Record
-        $user = new \App\Models\User();
-        $user->name = 'Salumao';
-        $user->email= 'salubcosta@gmail.com';
-        $user->password= bcrypt('123');
-        return \App\Models\User::all();
-    */
-
-    /*
-        $user = \App\Models\User::find(81);
-        $user->name = 'Salumão Editado';
-        $user->save();
-        return \App\Models\User::all();
-    */
-
-    /**
-     * Retorna todos os usuários
-     * return \App\Models\User::All(); 
-     * 
-     * Retorna usuários específico
-     * return \App\Models\User::find(81);
-     * 
-     * Retorno com where: 
-     *             +get     - retorna array
-     *             +first   - retorna um objeto
-     * return \App\Models\User::where('name', 'Salumao Editado')->get();   # collation
-     * return \App\Models\User::where('name', 'Salumao Editado')->first(); # object
-     * 
-     * Paginação
-     * return \App\Models\User::paginate(10);
-    */
-    
-    /*
-        Mass assignment - Atribuição em massa
-        $user = \App\Models\User::create([
-            'name'  => 'Salumão Barbosa',
-            'email' => 'salu.tj@hotmail.com',
-            'password' => bcrypt('123')
-        ]);
-
-        Mass update - Atualização em massa
-        $user = \App\Models\User::find(83);
-        $returnAction  = $user->update([
-            'name'  => 'Salumão BC',
-            'email' => 'salubcosta@outlook.com',
-            'password' => bcrypt('123')
-        ]);
-        dd($returnAction); # true or false
-    */
-    
-    return \App\Models\User::paginate(10);
-});
+Route::get('/product/{slug}', [HomeController::class, 'single'])->name('product.single');
 
 Route::prefix('admin')->name('admin.')->middleware('auth')->group(function(){
-    // Route::prefix('stores')->name('stores.')->group(function(){
-    //     Route::get('/', [StoreController::class, 'index'])->name('index');
-    //     Route::get('/create', [StoreController::class, 'create'])->name('create');
-    //     Route::post('/store', [StoreController::class, 'store'])->name('store');
-    //     Route::get('/{store}/edit', [StoreController::class, 'edit'])->name('edit');
-    //     Route::put('/update/{store}', [StoreController::class, 'update'])->name('update');
-    //     Route::get('/destroy/{store}',[StoreController::class, 'destroy'])->name('destroy');
-    // });
+ 
     Route::resource('stores', StoreController::class);
     Route::resource('products', ProductController::class);
     Route::resource('categories', CategoryController::class);
@@ -101,3 +34,79 @@ Route::prefix('admin')->name('admin.')->middleware('auth')->group(function(){
 Auth::routes();
 
 Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+
+
+
+
+
+// Route::get('/home', function () {
+//     $helloWorld = 'Hello World.';
+//     return view('welcome', compact('helloWorld'));
+// })->name('home');
+
+
+   // Route::prefix('stores')->name('stores.')->group(function(){
+    //     Route::get('/', [StoreController::class, 'index'])->name('index');
+    //     Route::get('/create', [StoreController::class, 'create'])->name('create');
+    //     Route::post('/store', [StoreController::class, 'store'])->name('store');
+    //     Route::get('/{store}/edit', [StoreController::class, 'edit'])->name('edit');
+    //     Route::put('/update/{store}', [StoreController::class, 'update'])->name('update');
+    //     Route::get('/destroy/{store}',[StoreController::class, 'destroy'])->name('destroy');
+    // });
+
+
+// Route::get('/model', function(){
+    
+//     /*  
+//         Utilizando o recurso chamado Active Record
+//         $user = new \App\Models\User();
+//         $user->name = 'Salumao';
+//         $user->email= 'salubcosta@gmail.com';
+//         $user->password= bcrypt('123');
+//         return \App\Models\User::all();
+//     */
+
+//     /*
+//         $user = \App\Models\User::find(81);
+//         $user->name = 'Salumão Editado';
+//         $user->save();
+//         return \App\Models\User::all();
+//     */
+
+//     /**
+//      * Retorna todos os usuários
+//      * return \App\Models\User::All(); 
+//      * 
+//      * Retorna usuários específico
+//      * return \App\Models\User::find(81);
+//      * 
+//      * Retorno com where: 
+//      *             +get     - retorna array
+//      *             +first   - retorna um objeto
+//      * return \App\Models\User::where('name', 'Salumao Editado')->get();   # collation
+//      * return \App\Models\User::where('name', 'Salumao Editado')->first(); # object
+//      * 
+//      * Paginação
+//      * return \App\Models\User::paginate(10);
+//     */
+    
+//     /*
+//         Mass assignment - Atribuição em massa
+//         $user = \App\Models\User::create([
+//             'name'  => 'Salumão Barbosa',
+//             'email' => 'salu.tj@hotmail.com',
+//             'password' => bcrypt('123')
+//         ]);
+
+//         Mass update - Atualização em massa
+//         $user = \App\Models\User::find(83);
+//         $returnAction  = $user->update([
+//             'name'  => 'Salumão BC',
+//             'email' => 'salubcosta@outlook.com',
+//             'password' => bcrypt('123')
+//         ]);
+//         dd($returnAction); # true or false
+//     */
+    
+//     return \App\Models\User::paginate(10);
+// });
